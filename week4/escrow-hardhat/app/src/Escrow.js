19d2020375ed0da2f716301566ed30ba8fc2bb01
rg-softwares <rochegrup.softwares@gmail.com>
2023-04-11 15:42:15 +0200

    [ADD]: project week4

---
+++ week4/escrow-hardhat/app/src/Escrow.js
@@ -0,0 +1,43 @@
+import { ethers } from "ethers";
+
+export default function Escrow({
+  address,
+  arbiter,
+  beneficiary,
+  value,
+  handleApprove,
+}) {
+  return (
+    <div className="existing-contract">
+      <ul className="fields">
+        <li>
+          <div> Contract address </div>
+          <div> {address} </div>
+        </li>
+        <li>
+          <div> Arbiter </div>
+          <div> {arbiter} </div>
+        </li>
+        <li>
+          <div> Beneficiary </div>
+          <div> {beneficiary} </div>
+        </li>
+        <li>
+          <div> Value </div>
+          <div> {ethers.utils.formatEther(value)} </div>
+        </li>
+        <div
+          className="button"
+          id={address}
+          onClick={(e) => {
+            e.preventDefault();
+
+            handleApprove();
+          }}
+        >
+          Approve
+        </div>
+      </ul>
+    </div>
+  );
+}
